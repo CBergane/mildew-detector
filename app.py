91acbd12abc3c795ee31f6e208afc3364ffb5fbd
CBergane <christian.bergane@gmail.com>
2023-04-18 11:00:30 +0000

    chaned spelling error

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from app_pages.multi_page import MultiPage
 
 from app_pages.page_summary import page_summary_body
 from app_pages.page_hypothesis import page_hypothesis_body
-from app_pages.leaf_visualizer import page_leaf_visualizer_body
+from app_pages.leaf_visualiser import page_leaf_visualiser_body
 from app_pages.performance import page_performance_body
 from app_pages.mildew_detector import page_mildew_detection_page
 
@@ -12,7 +12,7 @@ app = MultiPage(app_name = "Cherry Leaf Mildew Detector")
 
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Hypothesis and Visualization", page_hypothesis_body)
-app.add_page("Cherry Leaf Visualizer", page_leaf_visualizer_body)
+app.add_page("Cherry Leaf Visualizer", page_leaf_visualiser_body)
 app.add_page('ML Performance Metric', page_performance_body)
 app.add_page('Mildew Detector', page_mildew_detection_page)
 
