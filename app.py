72ee34cbdc65f48c902ed253302db401220a3993
CBergane <christian.bergane@gmail.com>
2023-05-05 11:08:48 +0000

    updated the app.py page with links for the new usage page

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ from app_pages.page_hypothesis import page_hypothesis_body
 from app_pages.leaf_visualiser import page_leaf_visualiser_body
 from app_pages.performance import page_performance_body
 from app_pages.mildew_detector import page_mildew_detection_page
+from app_pages.page_usage import page_usage_body
 
 
 app = MultiPage(app_name = "Cherry Leaf Mildew Detector")
 
 app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Project Usage", page_usage_body)
 app.add_page("Hypothesis and Visualization", page_hypothesis_body)
 app.add_page("Cherry Leaf Visualizer", page_leaf_visualiser_body)
-app.add_page('ML Performance Metric', page_performance_body)
-app.add_page('Mildew Detector', page_mildew_detection_page)
+app.add_page("ML Performance Metric", page_performance_body)
+app.add_page("Mildew Detector", page_mildew_detection_page)
+
 
 app.run()
