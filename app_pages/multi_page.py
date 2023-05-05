51fddad238bb75ccf5c136796b9c03beb6d5ae0b
CBergane <christian.bergane@gmail.com>
2023-05-05 12:25:24 +0000

    added a background for the multipage

--- app_pages/multi_page.py
+++ app_pages/multi_page.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
+
+bckgr_img = 'https://www.kindpng.com/picc/m/37-371135_japanese-flowering-cherry-png-clipart-cherry-blossoms-no.png" alt="Japanese Flowering Cherry Png Clipart - Cherry Blossoms No Background, Transparent Png@kindpng.com'
+
+# Define CSS style
+def set_background(image):
+    style = f"""
+    <style>
+    .stApp {{
+        background-image: url("{image}");
+        background-size: 18%;
+        background-position: top right;
+        background-repeat: no-repeat;
+    }}
+    </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
+    
 class MultiPage:
 
     def __init__(self, app_name) -> None:
