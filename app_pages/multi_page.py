e91bf866bc505049659fc9bd3c144765b486c4a9
CBergane <christian.bergane@gmail.com>
2023-05-05 22:28:32 +0000

    trying something new with bg, loads it from local

--- app_pages/multi_page.py
+++ app_pages/multi_page.py
@@ -1,5 +1,28 @@
 import streamlit as st
+import base64
 
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('images/cherry.png')
 
 bckgr_img = 'https://i.pinimg.com/564x/96/f8/80/96f8800ed069252f58d52937c7d92572.jpg'
 
@@ -10,7 +33,7 @@ def set_background(image):
     <style>
     .stApp {{
         background-image: url("{image}");
-        background-size: 18%;
+        background-size: 100%;
         background-position: top right;
         background-repeat: no-repeat;
     }}
