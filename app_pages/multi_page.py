e7c18b61559d9906b2ac886c36e9002c604987dc
CBergane <christian.bergane@gmail.com>
2023-05-05 22:34:14 +0000

    trying something new with bg, loads it from local

--- app_pages/multi_page.py
+++ app_pages/multi_page.py
@@ -24,22 +24,7 @@ def set_png_as_page_bg(png_file):
 
 set_png_as_page_bg('images/cherry.png')
 
-bckgr_img = 'https://i.pinimg.com/564x/96/f8/80/96f8800ed069252f58d52937c7d92572.jpg'
 
-
-# Define CSS style
-def set_background(image):
-    style = f"""
-    <style>
-    .stApp {{
-        background-image: url("{image}");
-        background-size: 100%;
-        background-position: top right;
-        background-repeat: no-repeat;
-    }}
-    </style>
-    """
-    st.markdown(style, unsafe_allow_html=True)
 
 class MultiPage:
 
