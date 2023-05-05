dab23468cd55e8d1cb7b367c2274c63f987c3744
CBergane <christian.bergane@gmail.com>
2023-05-05 22:49:27 +0000

    removed img tries, not working the way i whant it to

--- app_pages/multi_page.py
+++ app_pages/multi_page.py
@@ -1,47 +1,29 @@
+'''
+This file and the contents have been taken from
+ the Churnometer Walkthrough Project 2
+'''
 import streamlit as st
-import base64
-
-
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg('images/cherry.png')
-
 
 
 class MultiPage:
-
+    '''
+    Class to generate multiple Streamlit pages
+    using an object oriented approach
+    '''
     def __init__(self, app_name) -> None:
         self.pages = []
         self.app_name = app_name
 
         st.set_page_config(
             page_title=self.app_name,
-            page_icon="🖥️")
+            page_icon="🏡")
 
-        
-    
-    def add_page(self, title, func) -> None: 
-        self.pages.append({"title": title, "function": func })
+    def add_page(self, title, func) -> None:
+        self.pages.append({"title": title, "function": func})
 
     def run(self):
         st.title(self.app_name)
-        page = st.sidebar.radio('Menu', self.pages, format_func=lambda page: page['title'])
-        page['function']() 
+        page = st.sidebar.radio('Menu',
+                        self.pages,
+                        format_func=lambda page: str(page['title']))
+        page['function']()
